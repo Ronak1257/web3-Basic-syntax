e36e6776e87e91505fdaa2384f0dd0f72ec3269b
Ronak Bharadiya <ronakbharadiya1257@gmail.com>
2024-01-15 15:16:21 +0530

    Update HelloWorld.sol

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-contract SimpleStorage {
+contract Token {
     //create uint variable with initial value of 10
     function getVal() public view returns (uint) {
         //return that variable with return keyword suppose your variable is a
